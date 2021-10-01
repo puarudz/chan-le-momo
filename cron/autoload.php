881abc9f659699df5efe2e48e13dc3306a8da729
Vĩnh Dragon <58975846+vudinhnamkhanh@users.noreply.github.com>
2021-10-01 15:31:05 +0700

    Chú thích vào mã nguồn về việc mục đích sư dụng
    Đã kiểm tra không có lỗi & thêm chú thích về mục đích sử dụng mã nguồn
    @vudinhnamkhanh

--- cron/autoload.php
+++ cron/autoload.php
@@ -1,7 +1,9 @@
 <?php
     require_once("../config/config.php");
     require_once("../config/function.php");
-
+    
+    /* Không sử dụng mã nguồn để KINH DOANH CỜ BẠC BẤT HỢP PHÁP */
+    
     foreach($CMSNT->get_list(" SELECT DISTINCT `partnerId` FROM `momo` ") as $row)
     {
         $total_money = $CMSNT->get_row(" SELECT SUM(`amount`) FROM `momo` WHERE `partnerId` = '".$row['partnerId']."' ")['SUM(`amount`)'];
